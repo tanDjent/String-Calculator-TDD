bdbfa75eeeec8bd9763698d33320d21a174ae7a2
Tanmay Verma <vermatanmay3@gmail.com>
2021-02-13 16:30:44 +0530

    Delimiter

--- stringCalculator.java
+++ stringCalculator.java
@@ -1,18 +1,20 @@
+import java.util.Scanner;
 class StringCalculator{
     public int add(String numbers){
-        int sum,i,beginning;
+        int sum;
+        String n;
         if(numbers.length()==0)return 0;
-        i=0;
+        Scanner num=new Scanner(numbers);
+        num.useDelimiter("[;,\n]");
         sum=0;
-        beginning=0;
-        while(i<numbers.length()){
-            if(numbers.charAt(i)==','||numbers.charAt(i)=='\n'){
-                sum+=Integer.parseInt(numbers.substring(beginning,i));
-                beginning=i+1;
-            }
-            ++i;
+        while(num.hasNext()){
+            n=num.next();
+            if(n.length()==0)continue;
+            if(n.charAt(0)<'0' || n.charAt(0)>'9')continue;
+            sum+=Integer.parseInt(n);
         }
-        sum+=Integer.parseInt(numbers.substring(beginning,i));
+        num.close();
+     
         return sum;
     }
 }
@@ -24,11 +26,14 @@ class Main{
         String c="3,4";
         String d="1,2,3,4,5,6,7,8,9";
         String e="1\n2,3,4\n5";
-    
+        String f="//;\n1;2"; 
+
         System.out.println(s.add(a));
         System.out.println(s.add(b));
         System.out.println(s.add(c));
         System.out.println(s.add(d));
         System.out.println(s.add(e));
+        System.out.println(s.add(f));
+      
     }
 }
