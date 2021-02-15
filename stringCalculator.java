1c85714faf2f454ca460a4a48d74bcf16d7eb01a
Tanmay Verma <vermatanmay3@gmail.com>
2021-02-15 18:43:50 +0530

    Delimiter Correction

--- stringCalculator.java
+++ stringCalculator.java
@@ -1,12 +1,23 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import MyExceptions.NegativeNumberException;
 class StringCalculator{
+    private int callCount;
+    public StringCalculator(){
+        this.callCount=0;
+    }
     public int add(String numbers){
+        this.callCount++;
         int sum,parsedNumber;
         String token;
+        ArrayList<Integer> negativeNumbers=new ArrayList<Integer>();
+        NegativeNumberException n;
         if(numbers.length()==0)return 0;
         Scanner num=new Scanner(numbers);
-        num.useDelimiter("[;,\n]");
+        if(numbers.charAt(0)=='/' && numbers.charAt(1)=='/' && numbers.charAt(3)=='\n'){
+            num.useDelimiter("["+numbers.charAt(2)+",\n]");
+        }
+        else num.useDelimiter("[,\n]");
         sum=0;
         while(num.hasNext()){
             token=num.next();
@@ -14,14 +25,19 @@ class StringCalculator{
             if((token.charAt(0)<'0' || token.charAt(0)>'9') && token.charAt(0)!='-')continue;
             parsedNumber=Integer.parseInt(token);
             if(parsedNumber<0){
-                NegativeNumberException n= new NegativeNumberException(parsedNumber);
-                num.close();
-                throw n;
+                negativeNumbers.add(parsedNumber);
             }
             sum+=parsedNumber;          
          }
         num.close();
+        if(negativeNumbers.size()!=0){
+            n=new NegativeNumberException(negativeNumbers);
+            throw n;
+        }
         return sum;
+    }
+    public int getCalledCount(){
+        return this.callCount;
     }
 }
 class Main{
@@ -33,7 +49,7 @@ class Main{
         String d="1,2,3,4,5,6,7,8,9";
         String e="1\n2,3,4\n5";
         String f="//;\n1;2"; 
-        String g="1,2,3,4,5,6,7,8,-9";
+        String g="1,2,3,-4,5,6,7,8,-9";
 
        try{ System.out.println(s.add(a));
         System.out.println(s.add(b));
@@ -41,7 +57,9 @@ class Main{
         System.out.println(s.add(d));
         System.out.println(s.add(e));
         System.out.println(s.add(f));
+        System.out.println("CallCount: "+ s.getCalledCount());
         System.out.println(s.add(g));
+        System.out.println("CallCount: "+ s.getCalledCount());
     }catch(NegativeNumberException negativeNumberException){
         System.out.println(negativeNumberException.getMessage());
     }
