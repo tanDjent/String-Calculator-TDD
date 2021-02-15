e724fc0ec372cf673fb91091ce3c9f0ffc280c4b
Tanmay Verma <vermatanmay3@gmail.com>
2021-02-15 09:06:03 +0530

    Multiple Negatives

--- MyExceptions/NegativeNumberException.java
+++ MyExceptions/NegativeNumberException.java
@@ -1,9 +1,15 @@
 package MyExceptions;
-
+import java.util.ArrayList;
 public class NegativeNumberException extends RuntimeException {
     private String message;
     public NegativeNumberException(int num){
         this.message="Negatives not allowed: "+num;
+    }
+    public NegativeNumberException(ArrayList<Integer> negativeNumbers){
+        this.message="Negatives not allowed: ";
+        for (Integer integer : negativeNumbers) {
+                this.message=this.message+integer+" ";            
+        }
     }
     public String getMessage(){
         return this.message;
